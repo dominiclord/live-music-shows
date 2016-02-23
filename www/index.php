ddde0a28ddb37379062d14bf83179ea7739d018e
dominiclord <dlord@outlook.com>
2016-02-23 00:44:41 -0500

    Blues Skies Turn Black is scrapable

--- www/index.php
+++ www/index.php
@@ -45,6 +45,8 @@ $app->get('/[{foo}]', function ($request, $response, $args) {
             $links[] = $node->attr('href');
         });
 
+    $links = [];
+
     foreach ($links as $link) {
         $show_crawler = $client
             ->request('GET', $link)
@@ -138,94 +140,52 @@ $app->get('/[{foo}]', function ($request, $response, $args) {
      * @todo Add some type of logging for failed scrapings
      */
     $links = [
-        //'http://blueskiesturnblack.com/shows?page=0',
-        //'http://blueskiesturnblack.com/shows?page=1'
+        'http://blueskiesturnblack.com/shows?page=0',
+        'http://blueskiesturnblack.com/shows?page=1'
     ];
 
     foreach ($links as $link) {
         $show_crawler = $client
             ->request('GET', $link)
-            ->filter('#content > .post > .post-entry > p')
+            ->filter('.view-shows > .view-content > .views-row')
             ->each(function ($node) use (&$nodes) {
+                // This node contains date & time and artists, nothing else interests us
+                // Date is relatively straight forward stuff
+                $date = trim($node->filter('.views-field-nothing .date-display-single')->text());
+                // For some reason, they are displaying times in 24h format WITH 12h periods
+                // Lazy removal, could break things
+                $time = str_replace(['am','pm'], '', trim($node->filter('.views-field-nothing .showtime')->text()));
+                // Generating a DateTime object using the combination of $time and $date string
+                $datetime = new \DateTime($date . ', ' . $time);
 
-                // Okay, let's try to sort that
-                $split_content = preg_split('/<br[^>]*>/i', $node->html());
-
-                // Majority of shows are 4 lines. If less or more, fuck that, not dealing with you
-                // Or worse yet, format has changed. Abandon ship
-                if (count($split_content) === 4) {
-
-                    // Splitting line 1 (bilingual date strings)
-                    $date_array = preg_split('/\|/', $split_content[0]);
-                    // We'll use the english one for simplicity's sake (normally at end)
-                    $date = end($date_array);
-                    // Sometime months will be in the wrong language anyways
-                    $date = str_replace(
-                    [
-                        'janvier',
-                        'février',
-                        'mars',
-                        'avril',
-                        'mai',
-                        'juin',
-                        'juillet',
-                        'août',
-                        'septembre',
-                        'octobre',
-                        'novembre',
-                        'décembre'
-                    ],
-                    [
-                        'january',
-                        'february',
-                        'march',
-                        'april',
-                        'may',
-                        'june',
-                        'july',
-                        'august',
-                        'september',
-                        'october',
-                        'november',
-                        'december'
-                    ], $date);
-
-                    // Splitting line 2 (artists)
-                    $artists_array = preg_split('/\|/', $split_content[1]);
-                    $artists = [];
-
-                    foreach ($artists_array as $artist) {
-                        $artists[] = [
-                            'name' => trim($artist)
-                        ];
-                    }
-
-                    // Splitting line 3 (location location location)
-                    // For now, I don't give a shit about adresses
-                    $location_array = preg_split('/(:)/', $split_content[2]);
-                    $location = trim(current($location_array));
-
-                    // Splitting line 4 (price, time)
-                    // Usually, index [1] is a useless string
-                    // Just in case it's not there, we'll get around it by fetching both extremities
-                    $details_array = preg_split('/\|/', $split_content[3]);
-                    $price = preg_replace('/[^\d,\.]/', '', trim(current($details_array)));
-                    $price = preg_replace('/,(\d{2})$/', '.$1', $price);
-                    $time = trim(str_replace(['Doors','Portes','@'], '', end($details_array)));
-
-                    // Generating a DateTime object using the combination of $time and date string
-                    $datetime = new \DateTime($date . ' ' . $time);
-
-                    $nodes[] = [
-                        'timestamp' => $datetime->getTimestamp(),
-                        'date' => $datetime->format('d F'),
-                        'time' => $datetime->format('H:i'),
-                        'artists_string' => $split_content[1],
-                        'artists' => $artists,
-                        'location' => $location,
-                        'price' => $price
+                // Fetching artists
+                $artists = [];
+                // 4 artists or less are output, but sometimes are simply empty markup
+                $artists_array = $node->filter('.views-field-nothing [class^="act"] a')->each(function ($artist_node) use (&$artists) {
+                    $artists[] = [
+                        'name' => trim($artist_node->text())
                     ];
-                }
+                });
+
+                // This node contains location and pricing
+                // Location is tricky since there is nearly always a Google Maps link somewhere in there
+                $location_array = preg_split('/<br[^>]*>/i', $node->filter('.views-field-nothing-1 .location')->html());
+                $location = trim(current($location_array));
+
+                // Fetching price
+                // Opinionated decision to treat door price as official price
+                // Gotta skirt around that annoying <strong> around the label. Goutte and a nice regex have got us covered
+                $price = preg_replace('/[^\d,\.]/', '', trim($node->filter('.views-field-nothing-1 .doorPrice')->text()));
+                $price = preg_replace('/,(\d{2})$/', '.$1', $price);
+
+                $nodes[] = [
+                    'timestamp' => $datetime->getTimestamp(),
+                    'date' => $datetime->format('d F'),
+                    'time' => $datetime->format('H:i'),
+                    'artists' => $artists,
+                    'location' => $location,
+                    'price' => $price
+                ];
             });
     }
 
